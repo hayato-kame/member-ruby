c0ae2e84c411d9f3693360fbcd124f8bfc60bd87
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-07 01:29:39 +0000

    session 8.2

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,7 +10,8 @@ class Member < ApplicationRecord
     uniqueness: true
     
   validates :name, presence: true,
-    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
+    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true,
+            message: :invalid_member_name},
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
     

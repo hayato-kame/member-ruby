4532eba8eff36bb4820db123ed9efb8b0672e1e4
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-09 11:15:37 +0000

    article show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,8 @@ class ArticlesController < ApplicationController
       @articles = Article.order(released_at: :desc)
   end 
   
+  def show
+      @article = Article.find(params[:id])
+  end
+  
 end

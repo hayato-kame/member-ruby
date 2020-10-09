bf6134856efa3aaba7f24a6bb9fa7520f5c01477
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-09 08:19:09 +0000

    news index ok 332

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,10 @@ class ArticlesController < ApplicationController
   
   before_action :login_required, except: [:index, :show]
   
+  
+  # 記事一覧
+  def index
+      @articles = Article.order(released_at: :desc)
+  end 
+  
 end

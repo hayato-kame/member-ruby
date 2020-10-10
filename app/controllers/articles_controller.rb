69f9c3417762aec6c40f7d3423af35ae70ba5ae5
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-10 02:50:13 +0000

    scope model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,39 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end 
   
+  # 新規作成
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を登録しました。"
+    else
+      render "new"
+    end
+  end
+  
+  # 更新
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(article_params)
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を更新しました。"
+    else
+      render "edit"
+    end
+  end
+  
+  # 削除
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
+  end
+  
+  private
+  
+  # Strong Paramater
+  def article_params
+    params.require(:article).permit(:title, :body, :released_at, :no_expiration, :expired_at, :member_only)
+  end 
+  
 end

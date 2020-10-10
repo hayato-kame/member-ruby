5f9b7f57e75be47aaf840e59bbb4ecc1ab51f1d9
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-10 00:33:30 +0000

    article new edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,19 @@ class ArticlesController < ApplicationController
       @articles = Article.order(released_at: :desc)
   end 
   
+  # 記事の詳細表示
   def show
       @article = Article.find(params[:id])
   end
+  
+  # 新規登録フォーム
+  def new
+    @article = Article.new
+  end
+  
+  # 編集フォーム
+  def edit
+    @article = Article.find(params[:id])
+  end 
   
 end

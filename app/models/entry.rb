abb4bd9280b24c7d0f3a2f40b4248df83c47c76d
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-11 00:55:40 +0000

    entry seed_data OK

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,7 +5,30 @@ class Entry < ApplicationRecord
   STATUS_VALUES = %w(draft member_only public)
   
   validates :title, presence: true, length: { maximum: 200 }
+  
   validates :body, :posted_at, presence: true
+  
   validates :status, inclusion: { in: STATUS_VALUES }
+  
+  scope :common, -> { where(status: "public") }
+  
+  scope :published, -> { where("status <> ?", "draft") }
+  
+  scope :full, ->(member) {
+    where("status <> ? OR member_id = ?", "draft", member.id) }
+  
+  scope :readable_for, ->(member) { member ? full(member) : common }
+  
+  
+  # クラスメソッドの定義 class << self  から  end   までの間には いくらでもクラスメソッドが書けます。
+  class << self
+    def status_text(status)
+      I18n.t("activerecord.attributes.entry.status_#{status}")
+    end
+    
+    def status_options
+      STATUS_VALUES.map { |status| [status_text(status), status]}
+    end
+  end
   
 end
---
+++ db/seeds/development/entries.rb
@@ -0,0 +1,21 @@
+body = 
+  "今夜は久しぶりに神宮で野球観戦。\n\n" +
+  "先発はヤクルトが。。。。" +
+  "2回裏に。" +
+  "そして、8回裏には。\n\n" +
+  "ブキャナン投手の今期の。"
+  
+  
+%w(Taro Jiro Hana).each do |name|
+  member = Member.find_by(name: name)
+  
+  0.upto(9) do |idx|
+    Entry.create(
+      author: member,
+      title: "野球観戦#{idx}",
+      body: body,
+      posted_at: 10.days.ago.advance(days: idx),
+      status: %w(draft member_only public)[idx % 3]
+      )
+  end
+end
